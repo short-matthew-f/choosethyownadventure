3664178714aa2984584fcdf85f6f331ce10da579
Matthew F Short <short.matthew.f@gmail.com>
2014-09-19 14:54:26 -0400

    index improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def user_rating_class(user)
+    mazes = user.mazes
+    
+    return "none" if mazes.count == 0
+    
+    %w(none one two three four five)[(mazes.map(&:average_rating).inject(:+) / mazes.count).to_i]
+  end
+  
+  def win_percentage(user)
+    histories = user.histories
+    
+    return 0 unless histories.any?
+    
+    wins = user.histories.map(&:win_count).inject(:+)
+    losses = user.histories.map(&:loss_count).inject(:+)
+    
+    return (100 * wins) / (wins + losses)
+  end
 end
