012289b772d12b05572df7b7c83202b1d8f5bc92
Matthew F Short <short.matthew.f@gmail.com>
2014-09-19 11:00:01 -0400

    sprites

--- app/helpers/mazes_helper.rb
+++ app/helpers/mazes_helper.rb
@@ -2,4 +2,8 @@ module MazesHelper
   def author_name(maze)
     maze.author.profile.name
   end
+  
+  def rating_class(maze)
+    %w(none one two three four five)[maze.average_rating.to_i]
+  end
 end
